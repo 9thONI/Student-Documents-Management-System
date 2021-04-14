0ac28fbdb4b4bfe9df424ea75f40fd6e65778a80
Your Name <you@example.com>
2021-04-14 22:51:57 +0800

    initial commit - My System in oop

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StudentDocumentsManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter username: ");
+            string uName = Console.ReadLine();
+            Console.Write("Enter Password: ");
+            string pWord = Console.ReadLine();
+            if (uName == "admin" & pWord == "admin")
+            {
+                Console.WriteLine("Hello Admin");
+
+            }
+            else
+                Console.WriteLine("INVALID INPUT");
+        }
+    }
+}
