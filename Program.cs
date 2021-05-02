620d78ecf02209371ad5f5be0aa9a6c565d609e5
Your Name <you@example.com>
2021-05-02 12:15:04 +0800

    Act2:ImplementList&Methods

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace StudentDocumentsManagementSystem
 {
@@ -10,47 +9,141 @@ namespace StudentDocumentsManagementSystem
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter username: ");
-            string uName = Console.ReadLine();
-            Console.Write("Enter Password: ");
-            string pWord = Console.ReadLine();
-            if (uName == "admin" & pWord == "admin")
+            string adminUser = "admin";
+            string adminPassword = "admin";
+            string AdminU, AdminPass;
+            Console.WriteLine("|------------------------------------------------------------------|");
+            Console.WriteLine("|-----Welcome to angeliko's Student Document Management System-----|");
+            Console.WriteLine("|------------------------------------------------------------------|");
+            Console.Write("Username: ");
+            AdminU = Console.ReadLine();
+            Console.Write("Password: ");
+            AdminPass = Console.ReadLine();
+            if (AdminU == adminUser && AdminPass == adminPassword)
             {
-                Console.WriteLine("|============================================|");
-                Console.WriteLine("|             You are logged in              |");
-                Console.WriteLine("|============================================|");
-                List<string> Names = new List<string>();
-                Names.Add("Angeliko Roland Centeno");
-                Names.Add("John Patrick Macalalad");
-                Names.Add("James Darrel Advincula");
-                Names.Add("Nestor Gerona");
-                Names.Add("Josel Catalan");
-                Names.Add("John Edward Barreras");
-               
-                foreach (string name in Names)
-                {
-                    Console.WriteLine("Student name: " + name);
-                }
-                Console.WriteLine("Enter student Name: ");
-                string inputName = Console.ReadLine();
-    
-                
-                if (inputName == (""))
-                {
-                    Console.WriteLine("Please Select a Student");
-                }
-                else if (Names.Any(s => s.Equals(inputName,StringComparison.OrdinalIgnoreCase)))
-                {
-                    Console.WriteLine("Documents not yet available");
 
-                }
-                else {
-                    Console.WriteLine("Student Not Found");
-                }
+                options();
+
+
             }
             else
+            {
                 Console.WriteLine("INVALID INPUT");
-            Console.ReadLine();
+               
+            }
+            static void options()
+            {
+                Console.WriteLine("|------------------------------------------------------------------|");
+                Console.WriteLine("|--------------- Select between the options L and E ---------------|");
+                Console.WriteLine("|------------------------------------------------------------------|");
+                Console.WriteLine(" L - View Lists Of Students");
+                Console.WriteLine(" E - Exit");
+                char option = Convert.ToChar(Console.ReadLine());
+                option = char.ToUpper(option);
+
+                switch (option)
+                {
+                    case 'L':
+                        viewStudents();
+                        break;
+                    case 'E':
+                        exit();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+
+                static void viewStudents()
+                {
+
+                    List<string> studName = new List<string>();
+                    Console.WriteLine("|------------------------------------------------------------------|");
+                    Console.WriteLine("|-------------------------- Student List --------------------------|");
+                    Console.WriteLine("|------------------------------------------------------------------|");
+                    studName.Add("Angeliko Roland Centeno");
+                    studName.Add("John Patrick Macalalad");
+                    studName.Add("James Darrel Advincula");
+                    studName.Add("Nestor Gerona");
+                    studName.Add("Josel Catalan");
+                    studName.Add("John Edward Barreras");
+
+                    foreach (string name in studName)
+                    {
+
+                        Console.WriteLine(name);
+
+                    }
+
+                    Console.WriteLine("\nEnter Student Name: ");
+                    string inputName = Console.ReadLine();
+
+                    if (inputName == (""))
+                    {
+                        Console.WriteLine("Please Select a Student");
+                    }
+                    else if (studName.Any(s => s.Equals(inputName, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        viewFile();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Student Not Found");
+                    }
+                }
+                static void viewFile()
+                {
+                    Console.WriteLine("|------------------------------------------------------------------|");
+                    Console.WriteLine("|-------------- Select between the options V , B , E --------------|");
+                    Console.WriteLine("|------------------------------------------------------------------|");
+                    Console.WriteLine(" V - View Student Files ");
+                    Console.WriteLine(" B - Back to list");
+                    Console.WriteLine(" E - Exit");
+                    char option = Convert.ToChar(Console.ReadLine());
+                    option = char.ToUpper(option);
+
+                    switch (option)
+                    {
+                        case 'V':
+                            files();
+                            break;
+                        case 'B':
+                            viewStudents();
+                            break;
+                        case 'E':
+                            exit();
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid Input");
+                            break;
+                    }
+
+                }
+
+            }
+            static void files()
+            {
+
+                Console.WriteLine("All files are complete \n");
+
+                exit();
+            }
         }
+
+            static void exit()
+            {
+                  Console.WriteLine("|------------------------------------------------------------------|");
+                  Console.WriteLine("|---------------- The program is now Shutting down ----------------|");
+                  Console.WriteLine("|------------------------ Have a nice Day -------------------------|");
+                  Console.WriteLine("|------------------------------------------------------------------|");
+
+            System.Environment.Exit(0);
+            }
+
+        }
+
     }
-}
+
